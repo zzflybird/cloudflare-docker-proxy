331e2bcf04e113cc2b997e4232fbe592402002c8
Yqchilde <yqchilde@gmail.com>
2024-06-14 11:52:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.closeai.me": dockerHub,
+  "quay.closeai.me": "https://quay.io",
+  "gcr.closeai.me": "https://gcr.io",
+  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
+  "k8s.closeai.me": "https://registry.k8s.io",
+  "ghcr.closeai.me": "https://ghcr.io",
+  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.closeai.me": dockerHub,
 };
 
 function routeByHosts(host) {
